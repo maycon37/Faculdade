7a7a9a0fa86fd87dabb0650871f8e00d9450e766
maycon37 <maycon.rodrigues3712@gmail.com>
2022-09-12 20:34:53 -0300

    Java (Beta)

---
+++ LetsCode/ExJava/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String nome = "Maycon Rodrigues";
+        
+        System.out.println("Olá Mundo");
+        System.out.println("Bem vindo ao JAVA "+nome+"!");
+    }
+}
