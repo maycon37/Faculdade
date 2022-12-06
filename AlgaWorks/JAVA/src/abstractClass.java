94cdc4715e715cf6c4576f4ea22144738f9196b1
maycon37 <maycon.rodrigues3712@gmail.com>
2022-12-06 18:28:34 -0300

    JAVA

---
+++ AlgaWorks/JAVA/src/abstractClass.java
@@ -0,0 +1,10 @@
+
+public class abstractClass {
+	public abstractClass() {
+		System.out.println("This is an abstract class constructor!");
+	}
+	
+	public void aMethod() {
+		System.out.println("This is the method in the abstract class");
+	}
+}
