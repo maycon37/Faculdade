bd3b2278a257d4a7b2aab16b5bf46c71de536af2
maycon37 <maycon.rodrigues3712@gmail.com>
2023-09-14 17:53:49 -0300

    1.3

--- Back End - Curso Livre/primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/controller/ProdutoController.java
+++ Back End - Curso Livre/primeiro-exemplo/src/main/java/com/teste/primeiroexemplo/controller/ProdutoController.java
@@ -1,10 +1,21 @@
 package com.teste.primeiroexemplo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.primeiroexemplo.model.Produto;
+import com.teste.primeiroexemplo.services.ProdutoService;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+    public List<Produto> obterTodos(){
+        return produtoService.obterTodos();
+    }
     
 }
